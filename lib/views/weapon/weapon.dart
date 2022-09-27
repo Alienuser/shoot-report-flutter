d3b37fa7d27189bdd401ec92ae76fa5fa3ff369c
Lars Helmuth Probst <larsprobst@ibm.com>
2022-09-27 09:58:24 +0200

    Updaing Android.

--- lib/views/weapon/weapon.dart
+++ lib/views/weapon/weapon.dart
@@ -5,7 +5,6 @@ import 'package:shoot_report/services/training_dao.dart';
 import 'package:shoot_report/services/weapon_dao.dart';
 import 'package:shoot_report/views/weapon/weapon_list.dart';
 import 'package:shoot_report/widgets/ads.dart';
-import 'package:shoot_report/utilities/theme.dart';
 
 class WeaponWidget extends StatefulWidget {
   final WeaponDao weaponDao;
