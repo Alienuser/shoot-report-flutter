9ca607098ba29a255cc50da5d934acc36894b560
Lars Helmuth Probst <lars@famprobst.de>
2024-08-29 12:41:20 +0200

    Upgrade dependencies and fix weapon adding while migrating.

--- lib/utilities/app_migration.dart
+++ lib/utilities/app_migration.dart
@@ -78,96 +78,100 @@ class AppMigration {
   }
 
   static Future<int> addNewWeapons(FlutterDatabase database) async {
-    await database.weaponDao
-        .insertWeapon(Weapon(14, "weapon_13", 13, "prefWeapon13", 1, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(15, "weapon_14", 14, "prefWeapon14", 1, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(16, "weapon_15", 15, "prefWeapon15", 1, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(17, "weapon_16", 16, "prefWeapon16", 1, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(18, "weapon_17", 17, "prefWeapon17", 1, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(19, "weapon_18", 18, "prefWeapon18", 1, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(20, "weapon_19", 19, "prefWeapon19", 1, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(21, "weapon_20", 20, "prefWeapon20", 1, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(22, "weapon_21", 21, "prefWeapon21", 1, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(23, "weapon_22", 22, "prefWeapon22", 1, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(24, "weapon_23", 23, "prefWeapon23", 1, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(25, "weapon_24", 24, "prefWeapon24", 1, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(26, "weapon_25", 25, "prefWeapon25", 2, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(27, "weapon_26", 26, "prefWeapon26", 2, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(28, "weapon_27", 27, "prefWeapon27", 2, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(29, "weapon_28", 28, "prefWeapon28", 2, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(30, "weapon_29", 29, "prefWeapon29", 2, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(31, "weapon_30", 30, "prefWeapon30", 2, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(32, "weapon_31", 31, "prefWeapon31", 2, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(33, "weapon_32", 32, "prefWeapon32", 2, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(34, "weapon_33", 33, "prefWeapon33", 2, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(35, "weapon_34", 34, "prefWeapon34", 2, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(36, "weapon_35", 35, "prefWeapon35", 3, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(37, "weapon_36", 36, "prefWeapon36", 3, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(38, "weapon_37", 37, "prefWeapon37", 4, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(39, "weapon_38", 38, "prefWeapon38", 4, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(40, "weapon_39", 39, "prefWeapon39", 4, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(41, "weapon_40", 40, "prefWeapon40", 4, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(42, "weapon_41", 41, "prefWeapon41", 4, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(43, "weapon_42", 42, "prefWeapon42", 4, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(44, "weapon_43", 43, "prefWeapon43", 4, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(45, "weapon_44", 44, "prefWeapon44", 4, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(46, "weapon_45", 45, "prefWeapon45", 4, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(47, "weapon_46", 46, "prefWeapon46", 4, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(48, "weapon_47", 47, "prefWeapon47", 4, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(49, "weapon_48", 48, "prefWeapon48", 4, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(50, "weapon_49", 49, "prefWeapon49", 4, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(51, "weapon_50", 50, "prefWeapon50", 5, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(52, "weapon_51", 51, "prefWeapon51", 5, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(53, "weapon_52", 52, "prefWeapon52", 5, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(54, "weapon_53", 53, "prefWeapon53", 5, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(55, "weapon_54", 54, "prefWeapon54", 5, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(56, "weapon_55", 55, "prefWeapon55", 5, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(57, "weapon_56", 56, "prefWeapon56", 5, false));
-    await database.weaponDao
-        .insertWeapon(Weapon(58, "weapon_57", 57, "prefWeapon57", 5, false));
+    try {
+      await database.weaponDao
+          .insertWeapon(Weapon(14, "weapon_13", 13, "prefWeapon13", 1, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(15, "weapon_14", 14, "prefWeapon14", 1, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(16, "weapon_15", 15, "prefWeapon15", 1, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(17, "weapon_16", 16, "prefWeapon16", 1, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(18, "weapon_17", 17, "prefWeapon17", 1, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(19, "weapon_18", 18, "prefWeapon18", 1, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(20, "weapon_19", 19, "prefWeapon19", 1, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(21, "weapon_20", 20, "prefWeapon20", 1, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(22, "weapon_21", 21, "prefWeapon21", 1, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(23, "weapon_22", 22, "prefWeapon22", 1, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(24, "weapon_23", 23, "prefWeapon23", 1, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(25, "weapon_24", 24, "prefWeapon24", 1, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(26, "weapon_25", 25, "prefWeapon25", 2, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(27, "weapon_26", 26, "prefWeapon26", 2, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(28, "weapon_27", 27, "prefWeapon27", 2, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(29, "weapon_28", 28, "prefWeapon28", 2, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(30, "weapon_29", 29, "prefWeapon29", 2, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(31, "weapon_30", 30, "prefWeapon30", 2, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(32, "weapon_31", 31, "prefWeapon31", 2, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(33, "weapon_32", 32, "prefWeapon32", 2, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(34, "weapon_33", 33, "prefWeapon33", 2, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(35, "weapon_34", 34, "prefWeapon34", 2, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(36, "weapon_35", 35, "prefWeapon35", 3, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(37, "weapon_36", 36, "prefWeapon36", 3, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(38, "weapon_37", 37, "prefWeapon37", 4, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(39, "weapon_38", 38, "prefWeapon38", 4, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(40, "weapon_39", 39, "prefWeapon39", 4, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(41, "weapon_40", 40, "prefWeapon40", 4, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(42, "weapon_41", 41, "prefWeapon41", 4, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(43, "weapon_42", 42, "prefWeapon42", 4, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(44, "weapon_43", 43, "prefWeapon43", 4, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(45, "weapon_44", 44, "prefWeapon44", 4, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(46, "weapon_45", 45, "prefWeapon45", 4, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(47, "weapon_46", 46, "prefWeapon46", 4, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(48, "weapon_47", 47, "prefWeapon47", 4, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(49, "weapon_48", 48, "prefWeapon48", 4, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(50, "weapon_49", 49, "prefWeapon49", 4, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(51, "weapon_50", 50, "prefWeapon50", 5, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(52, "weapon_51", 51, "prefWeapon51", 5, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(53, "weapon_52", 52, "prefWeapon52", 5, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(54, "weapon_53", 53, "prefWeapon53", 5, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(55, "weapon_54", 54, "prefWeapon54", 5, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(56, "weapon_55", 55, "prefWeapon55", 5, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(57, "weapon_56", 56, "prefWeapon56", 5, false));
+      await database.weaponDao
+          .insertWeapon(Weapon(58, "weapon_57", 57, "prefWeapon57", 5, false));
+    } catch (_) {
+      log("Weapons are already existing.");
+    }
 
     return 0;
   }
